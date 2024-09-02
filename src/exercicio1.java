7b15e9860bcf45c6f85ecaff5154de29b7f24a46
melasioca <melasioca@gmail.com>
2024-09-02 16:45:50 -0300

    version1

---
+++ src/exercicio1.java
@@ -0,0 +1,11 @@
+public class exercicio1 {
+    public static void main(String[] args) {
+    int idade = 17;
+    float altura = 153;
+    double peso = 52;
+
+        System.out.println("idade:" + idade);
+        System.out.println("altura:" + altura);
+        System.out.println("peso:" + peso);
+    }
+}
