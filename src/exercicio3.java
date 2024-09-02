40a69b3817e60d1397276cc5ca523b3421763e8f
Melissa Lourenço de Souza <melasioca@gmail.com>
2024-09-02 17:11:07 -0300

    Update exercicio3.java

--- src/exercicio3.java
+++ src/exercicio3.java
@@ -1,8 +1,8 @@
 public class exercicio3 {
     public static void main(String[] args) {
         int n1 = 982804;
-        double n2 = (double) N1;
+        double n2 = (double) n1;
 
-        System.out.println(N2);
+        System.out.println(n2);
     }
 }
