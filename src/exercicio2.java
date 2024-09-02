bf6a328c715625a5c8b9f8bf493ff4583a460a00
melasioca <melasioca@gmail.com>
2024-09-02 16:54:14 -0300

    version1

---
+++ src/exercicio2.java
@@ -0,0 +1,10 @@
+public class exercicio2 {
+    public static void main(String[] args) {
+        int ht = 120;
+        int vp = 15;
+        int st = 0;
+        st = ht*vp;
+
+        System.out.println("O salário total é "+st);
+    }
+}
